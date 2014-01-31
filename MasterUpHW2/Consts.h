bd85502d9dd87bc9e1b92c74d9cde4d75988a7ad
@Kirill Ushkov <ushkov.k@dbbest.com>
2014-01-31 12:59:59 +0200

    added config and consts file

---
+++ MasterUpHW2/Consts.h
@@ -0,0 +1,21 @@
+//
+//  Consts.h
+//  MasterUpHW2
+//
+//  Created by Kirill on 1/31/14.
+//  Copyright (c) 2014 Администратор. All rights reserved.
+//
+
+#ifndef MasterUpHW2_Consts_h
+#define MasterUpHW2_Consts_h
+
+#pragma mark - Notifications - 
+
+#define didSelectRouteNotification  @"didSelectRoute"
+
+
+#pragma mark - Keys -
+
+#define kSelectedRoute @"kSelectedRoute"
+
+#endif
